65e20966acbd77bfa9cf6f2721b2a29324c3e20d
Dylan Martin <dylan.martin7675@gmail.com>
2019-12-18 19:36:45 -0800

    Added test script to confirm functionality

--- tests.sh
+++ tests.sh
@@ -1,7 +1,26 @@
-echo "******** testing adb push ********"
+echo "******** testing adb root ********"
+adbulk adb root &&
+echo "******** adb root passed ********"
 
-adbulk adb push test.txt &&
+echo "******** testing adb push ********"
+adbulk adb push test.txt /storage/emulated/0/Download &&
 echo "******** adb push passed ********"
 
+echo "******** testing adb push ********"
+adbulk adb pull /storage/emulated/0/Download/test.txt &&
+echo "******** adb pull passed ********"
 
+echo "******** testing adb install ********"
+adbulk adb install ~/Downloads/*.apk &&
+echo "******** adb install passed ********"
+
+echo "******** testing adb reboot ********"
+adbulk adb reboot bootloader &&
+echo "******** adb reboot passed ********"
+
+sleep 10
+
+echo "******** testing fastboot reboot ********"
+adbulk fastboot reboot &&
+echo "******** fastboot reboot passed ********"
 
